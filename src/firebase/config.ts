5995143729c942ae9f9568aab6fc9af0ff73203b
Prakash Babu paramhans <prakashbabuparamhans5g@gmail.com>
2026-03-17 13:31:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8470421013-409fd",
+  "appId": "1:341732618380:web:87ddf54b5776fc765b0ba9",
+  "apiKey": "AIzaSyCcvCp441fs1uViP4rK7CfK4YJ2LNm8nDA",
+  "authDomain": "studio-8470421013-409fd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "341732618380"
+};
